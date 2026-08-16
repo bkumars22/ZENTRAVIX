28a9990f453b2c42ad2aa534821d532de7d717c5
Kumara Swamy <swamy.kumar02@gmail.com>
2026-08-16 12:44:35 +0530

    Update next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -4,8 +4,8 @@ const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
-  basePath: isProd ? '/ZENTRAVIX' : '',
-  assetPrefix: isProd ? '/ZENTRAVIX/' : '',
+  basePath: '',
+  assetPrefix: '',
   images: { unoptimized: true },
 }
 module.exports = nextConfig
